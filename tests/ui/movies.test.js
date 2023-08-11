00a3c84206f3aaeae3011a6783dd648a3d75fc4e
VasilMG <119819354+VasilMG@users.noreply.github.com>
2023-08-11 13:02:32 +0300

    chage url tests

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-    await page.goto('http://localhost:8080/movies');
+    await page.goto('https://training-vasilmg-devops.onrender.com/movies');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
