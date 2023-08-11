bf4b9dadfe2efde23145f36ab656ec241249e675
VasilMG <119819354+VasilMG@users.noreply.github.com>
2023-08-11 13:08:32 +0300

    change

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
+
   await page.goto('https://training-vasilmg-devops.onrender.com/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
